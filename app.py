06967d3c52eb7879dd6756592f695e3d4f1a346b
Mengheng855 <mengheng.mh855@gmail.com>
2025-12-04 11:52:58 +0700

    .

--- app.py
+++ app.py
@@ -59,4 +59,4 @@ def save_photo():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=5000)
+    app.run(debug=False, host='0.0.0.0', port=5000) 
